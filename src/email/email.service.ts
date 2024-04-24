fb21f0418e0389b6aafd23d74e61522abd158e84
unknown <ifennamonanu70@gmail.com>
2024-04-24 12:53:47 +0100

    fix: fixed email template

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -20,31 +20,28 @@ async function sendDonationMail(prop: MailOptions) {
         <div style="width: 100%; background-color:#E9F8FF; height: 230px; border-radius:13px; display: flex; justify-content: space-between; align-items: center;">
             <img width="260px" height="228px" src="https://res.cloudinary.com/nigerianchurch/image/upload/v1713827601/y38x3kk2kmlnbnveclet.png" alt="img-person">
             <h1 style="width: 311px; margin-top: 50px; font-size: 32px;">
-                You have invested in someones Life!
+                You Have Made a Meaningful Impact!
             </h1>
         </div>
 
         <div style="padding: 25px 0px; border-color: #EFEFEF; border-bottom-width: 1px; border-bottom-style: solid;">
             <h1 style="font-size: 32px; padding-bottom: 12px;">Hello ${token}</h1>
             <p style="padding-bottom: 12px; font-size: 16px;">
-                You have invested in someones Life!
-            </p>
+                We are writing to express our heartfelt gratitude for your generous contribution. Your support is directly nurturing the aspirations of young, economically disadvantaged, and vulnerable individuals across Africa.            </p>
             <p style="padding-bottom: 12px; font-size: 16px;">
-                Thank you for your generosity towards improving Africa through our mission to raise more tech talent.
-            </p>
+                Thanks to you, we can continue to provide essential training and resources to countless young, economically disadvantaged, and vulnerable individuals. With every donation, we move closer to a world where everyone has the opportunity to learn, grow, and contribute to the technological landscape.            </p>
             <p style="padding-bottom: 12px; font-size: 16px;">
-                Every amount donated to this course will be used to empower young, economically disadvantaged, and vulnerable African women and youths.
-           </p>
+                We are thrilled to count you among our esteemed community of supporters. Your involvement is a powerful vote of confidence in our mission, and for that, we are immensely grateful.  </p>
            <p style="padding-bottom: 12px; font-size: 16px;">
-            Your support motivates us to keep up the good work.
-        </p>
+           Should you wish to share your thoughts or learn more about our ongoing projects, please don't hesitate to reach out at <a href="mailto: donation@genesysfoundation.org" >donation@genesysfoundation.org</a> or visit our website, <a href="www.genesysfoundation.org" style="font-weight: 500"}>www.genesysfoundation.org</a>.        </p>
         <p style="padding-bottom: 12px; font-size: 16px;">
-            We are deeply honored to have you as a member of our community, and together, we will make a difference.
+            Together, we are unlocking potential and inspiring innovation.
         </p>
         </div>
         <div style="padding: 16px 0px ;">
-            <p style="font-size: 16px;">Warm Regards</p>
-            <b style="font-size: 16px; margin-top: -4px">The Genesys Foundation Team</b>
+            <p style="font-size: 16px; padding-bottom: 15px; >Warm Regards </p>
+            <b style="font-size: 16px;">Nnamdi Anika</b>
+            <p style="font-size: 16px; margin-top: -4px">MD, Genesys Foundation</p>
         </div>
     </div>
 </body>
